50b58c417de5f0e2c438790e04ae0af6609ecdb6
Shaun Scovil <shaun.scovil@mediasilo.com>
2015-09-02 12:20:49 -0400

    Added uglified version of script; Added uglify gulp task and node dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,21 @@ gulp.task('lint', function() {
         .pipe($.jshint.reporter('jshint-stylish', {verbose: true}));
 });
 
+gulp.task('uglify', ['lint'], function() {
+    var rename = require('gulp-rename');
+    var uglify = require('gulp-uglify');
+
+    return gulp.src('./angular-material-icons.js')
+        .pipe(uglify())
+        .pipe(rename(dotMinExtension))
+        .pipe(gulp.dest('./'))
+    ;
+});
+
 gulp.task('default', ['lint']);
 
 module.exports = gulp;
+
+function dotMinExtension(path) {
+    path.extname = '.min' + path.extname;
+}
